e5c3a1c5e2303689d5e9936c2fc8a62070275e64
Vincenzo Palazzo <vincenzopalazzo1996@gmail.com>
2019-12-17 23:10:32 +0100

    update installer script

--- installer/update-submodule.sh
+++ installer/update-submodule.sh
@@ -1,4 +1,9 @@
 cd ..
 git submodule update --init --recursive
 git submodule update --recursive --remote --merge --force
+sudo rm -r include/spycblockrpc/lib/*
+cd include/spycblockrpc
+git submodule update --init --recursive
+cd ..
+cd ..
 cd installer
